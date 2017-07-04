4dc02bfc1a203a1c05d15051f0ff9c2eb0bef2c8
Zola <zolamcadie@gmail.com>
2017-07-04 22:08:47 +0000

    sets up server using ejs templates

--- express-server.js
+++ express-server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const app = express();
+// sets preffered port
 const PORT = process.env.PORT || 8080; // default port 8080
+
+// enables ejs templates (using render, etc...)
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+app.get('/', (req, res) => {
+  res.send('hello')
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// passes defined var to ejs template, displays with .render
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+// tells server to listen on pre-defined port
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Hello Seattle, Port: ${PORT} is listening.`);
 });
